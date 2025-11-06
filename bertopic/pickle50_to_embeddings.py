f5ac3b2ffe781c330bb202925db1170b4321e45c
nigmatov <noirchan@gmail.com>
2025-11-06 16:27:42 +0500

    Add files via upload

---
+++ bertopic/pickle50_to_embeddings.py
@@ -0,0 +1,20 @@
+import json
+import pickle
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit(1)
+
+embeddings = pickle.load(open(sys.argv[1], 'rb'))
+i = 0
+
+results = []
+
+with open(sys.argv[2] + 'bertopic50', 'w') as fout:
+    with open(sys.argv[2], 'r') as fin_docs:
+        for line in fin_docs:
+            line_json = json.loads(line)
+            line_json['_source']['topics_bertopic_50'] = embeddings[i]
+            fout.write(json.dumps(line_json))
+            fout.write('\n')
+            i += 1
